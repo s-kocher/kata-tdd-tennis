e96f4cd79a936178cb07e54885f04d70f095e16c
Sylvain K <skocher@avb202.com>
2021-05-06 18:27:17 +0200

    Game start - score Love-Love

--- src/main/java/fr/arolla/skocher/tennis/TennisGame.java
+++ src/main/java/fr/arolla/skocher/tennis/TennisGame.java
@@ -2,4 +2,7 @@ package fr.arolla.skocher.tennis;
 
 public class TennisGame {
 
+    public String score() {
+        return "Love-Love";
+    }
 }
--- src/test/java/fr/arolla/skocher/tennis/TennisGameTest.java
+++ src/test/java/fr/arolla/skocher/tennis/TennisGameTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 public class TennisGameTest {
 
     @Test
-    public void should_be_ok() {
-        assertEquals("1", "1");
+    public void should_game_start_score_must_be_Love_Love() {
+        TennisGame game = new TennisGame();
+
+        String score = game.score();
+
+        assertEquals("Love-Love", score);
     }
 
 }
