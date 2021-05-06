ffe0fdfd3976a8344b1f8dae900c726a30b8e9ca
Sylvain K <skocher@avb202.com>
2021-05-06 18:30:23 +0200

    For score 1-0 display score Fifteen-Love

--- src/main/java/fr/arolla/skocher/tennis/TennisGame.java
+++ src/main/java/fr/arolla/skocher/tennis/TennisGame.java
@@ -2,7 +2,18 @@ package fr.arolla.skocher.tennis;
 
 public class TennisGame {
 
+    private final int player1Score;
+    private final int player2Score;
+
+    public TennisGame(int p1Score, int p2Score) {
+        this.player1Score = p1Score;
+        this.player2Score = p2Score;
+    }
     public String score() {
+        if (player1Score == 1) {
+            return "Fifteen-Love";
+        }
+
         return "Love-Love";
     }
 }
--- src/test/java/fr/arolla/skocher/tennis/TennisGameTest.java
+++ src/test/java/fr/arolla/skocher/tennis/TennisGameTest.java
@@ -8,11 +8,20 @@ public class TennisGameTest {
 
     @Test
     public void should_game_start_score_must_be_Love_Love() {
-        TennisGame game = new TennisGame();
+        TennisGame game = new TennisGame(0, 0);
 
         String score = game.score();
 
         assertEquals("Love-Love", score);
+    }
+
+    @Test
+    public void should_first_player_scores_game_score_is_Fifteen_Love() {
+        TennisGame game = new TennisGame(1, 0);
+
+        String score = game.score();
+
+        assertEquals("Fifteen-Love", score);
     }
 
 }
