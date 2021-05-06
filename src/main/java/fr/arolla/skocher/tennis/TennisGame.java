eba84db8d53f95068832a16eda1a9a6cc3c19faf
Sylvain K <skocher@avb202.com>
2021-05-06 16:11:46 +0200

    Initialize

---
+++ src/main/java/fr/arolla/skocher/tennis/TennisGame.java
@@ -0,0 +1,5 @@
+package fr.arolla.skocher.tennis;
+
+public class TennisGame {
+
+}
---
+++ src/test/java/fr/arolla/skocher/tennis/TennisGameTest.java
@@ -0,0 +1,14 @@
+package fr.arolla.skocher.tennis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TennisGameTest {
+
+    @Test
+    public void should_be_ok() {
+        assertEquals("1", "1");
+    }
+
+}
