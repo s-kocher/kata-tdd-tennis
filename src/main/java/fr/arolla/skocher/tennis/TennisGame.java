1d3c7329ef516a2eda9c83d37c4ce4557f4dfa63
Sylvain K <skocher@avb202.com>
2021-05-07 15:53:11 +0200

    Refactor : display score constant + getDisplayedScore method

--- src/main/java/fr/arolla/skocher/tennis/TennisGame.java
+++ src/main/java/fr/arolla/skocher/tennis/TennisGame.java
@@ -2,6 +2,12 @@ package fr.arolla.skocher.tennis;
 
 public class TennisGame {
 
+    private static final String DISPLAY_SCORE_LOVE = "Love";
+    private static final String DISPLAY_SCORE_FIFTEEN = "Fifteen";
+    private static final String DISPLAY_SCORE_THIRTY = "Thirty";
+
+    private static final String DISPLAY_SCORE_SEPARATOR = "-";
+
     private final int player1Score;
     private final int player2Score;
 
@@ -12,19 +18,23 @@ public class TennisGame {
 
     public String score() {
         if (player1Score == 1 && player2Score == 1 ) {
-            return "Fifteen-Fifteen";
+            return getDisplayedScore(DISPLAY_SCORE_FIFTEEN, DISPLAY_SCORE_FIFTEEN);
         }
         if (player1Score == 1) {
-            return "Fifteen-Love";
+            return getDisplayedScore(DISPLAY_SCORE_FIFTEEN, DISPLAY_SCORE_LOVE);
         }
         if (player1Score == 2) {
-            return "Thirty-Love";
+            return getDisplayedScore(DISPLAY_SCORE_THIRTY, DISPLAY_SCORE_LOVE);
         }
         if (player2Score == 1) {
-            return "Love-Fifteen";
+            return getDisplayedScore(DISPLAY_SCORE_LOVE, DISPLAY_SCORE_FIFTEEN);
         }
 
-        return "Love-Love";
+        return getDisplayedScore(DISPLAY_SCORE_LOVE, DISPLAY_SCORE_LOVE);
+    }
+
+    private String getDisplayedScore(String displayScore1, String displayScore2) {
+        return displayScore1 + DISPLAY_SCORE_SEPARATOR + displayScore2;
     }
 
 }
