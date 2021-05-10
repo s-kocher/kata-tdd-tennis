b6cef8f8cadb769e1e7e7f5dbff3506f6384f099
Sylvain K <skocher@avb202.com>
2021-05-10 16:30:19 +0200

    Refactor using parametrized tests

--- src/test/java/fr/arolla/skocher/tennis/TennisGameTest.java
+++ src/test/java/fr/arolla/skocher/tennis/TennisGameTest.java
@@ -2,116 +2,42 @@ package fr.arolla.skocher.tennis;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class TennisGameTest {
 
-    @Test
-    public void should_for_raw_score_0_0_displayed_score_must_be_Love_Love() {
-        TennisGame game = new TennisGame(0, 0);
-
-        String score = game.score();
-
-        assertEquals("Love-Love", score);
+    static Stream<Arguments> inputPlayerScoresExpectedDisplayedScore () {
+        return Stream.of(
+            Arguments.of(0, 0, "Love-Love"),
+            Arguments.of(1, 0, "Fifteen-Love"),
+            Arguments.of(0, 1, "Love-Fifteen"),
+            Arguments.of(1, 1, "Fifteen-Fifteen"),
+            Arguments.of(2, 0, "Thirty-Love"),
+            Arguments.of(0, 2, "Love-Thirty"),
+            Arguments.of(2, 2, "Thirty-Thirty"),
+            Arguments.of(3, 0, "Forty-Love"),
+            Arguments.of(2, 3, "Thirty-Forty"),
+            Arguments.of(3, 3, "Deuce"),
+            Arguments.of(4, 4, "Deuce"),
+            Arguments.of(5, 5, "Deuce")
+//          Arguments.of(0, 0, "")
+        );
     }
 
-    @Test
-    public void should_for_raw_score_1_0_displayed_score_must_be_Fifteen_Love() {
-        TennisGame game = new TennisGame(1, 0);
+    @ParameterizedTest(name = "Raw player scores {0} - {1} => Display score game : {2} ")
+    @MethodSource("inputPlayerScoresExpectedDisplayedScore")
+    public void should_for_given_raw_player_scores_display_the_expected_game_score(
+        int player1Score, int player2Score, String expectedGameScore) {
+        TennisGame game = new TennisGame(player1Score, player2Score);
 
         String score = game.score();
 
-        assertEquals("Fifteen-Love", score);
-    }
-
-    @Test
-    public void should_for_raw_score_0_1_displayed_score_must_be_Love_Fifteen() {
-        TennisGame game = new TennisGame(0, 1);
-
-        String score = game.score();
-
-        assertEquals("Love-Fifteen", score);
-    }
-
-    @Test
-    public void should_for_raw_score_1_1_displayed_score_must_be_Fifteen_Fifteen() {
-        TennisGame game = new TennisGame(1, 1);
-
-        String score = game.score();
-
-        assertEquals("Fifteen-Fifteen", score);
-    }
-
-    @Test
-    public void should_for_raw_score_2_0_displayed_score_must_be_Thirty_Love() {
-        TennisGame game = new TennisGame(2, 0);
-
-        String score = game.score();
-
-        assertEquals("Thirty-Love", score);
-    }
-
-    @Test
-    public void should_for_raw_score_0_2_displayed_score_must_be_Love_Thirty() {
-        TennisGame game = new TennisGame(0, 2);
-
-        String score = game.score();
-
-        assertEquals("Love-Thirty", score);
-    }
-
-    @Test
-    public void should_for_raw_score_2_2_displayed_score_must_be_Thirty_Thirty() {
-        TennisGame game = new TennisGame(2, 2);
-
-        String score = game.score();
-
-        assertEquals("Thirty-Thirty", score);
-    }
-
-    @Test
-    public void should_for_raw_score_3_0_displayed_score_must_be_Forty_Love() {
-        TennisGame game = new TennisGame(3, 0);
-
-        String score = game.score();
-
-        assertEquals("Forty-Love", score);
-    }
-
-    @Test
-    public void should_for_raw_score_2_3_displayed_score_must_be_Thirty_Forty() {
-        TennisGame game = new TennisGame(2, 3);
-
-        String score = game.score();
-
-        assertEquals("Thirty-Forty", score);
-    }
-
-    @Test
-    public void should_for_raw_score_3_3_displayed_score_must_be_Deuce() {
-        TennisGame game = new TennisGame(3, 3);
-
-        String score = game.score();
-
-        assertEquals("Deuce", score);
-    }
-
-    @Test
-    public void should_for_raw_score_4_4_displayed_score_must_be_Deuce() {
-        TennisGame game = new TennisGame(4, 4);
-
-        String score = game.score();
-
-        assertEquals("Deuce", score);
-    }
-
-    @Test
-    public void should_for_raw_score_5_5_displayed_score_must_be_Deuce() {
-        TennisGame game = new TennisGame(5, 5);
-
-        String score = game.score();
-
-        assertEquals("Deuce", score);
+        assertEquals(expectedGameScore, score);
     }
 
 }
